8f2706b67fb11d69db2977b502ee72a7135bf253
John Gill <king2bgill@gmail.com>
2017-01-28 21:08:47 -0800

    Implement main game logic

--- game/main.cpp
+++ game/main.cpp
@@ -1,22 +1,92 @@
-#include "stdio.h"
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string>
+#include <iostream>
+#include <sstream>
 #include "board.h"
 
-int main()
-{
-    Board *b = new Board(10, 10, 10);
+using namespace std;
 
-    for(int i = 0; i < 2; i++)
+void usage(string error)
+{
+    cout << error << endl;
+    // TODO: Full "Help"
+    exit(1);
+}
+
+int main (int argc, char **argv)
+{
+    bool debug;
+    int nRow, nCol, nBomb, x, y;
+    char c, action;
+    nRow = nCol = nBomb = -1;
+    Board *b;
+    boardState_t gState = INPROGRESS;
+    stringstream ss;
+    string line;
+
+    opterr = 0;
+    while ((c = getopt (argc, argv, "r:c:b:d")) != -1)
     {
-        b->printBoard(1);
-        b->clickSquare(3, 0);
-        b->printBoard(1);
-        b->clickSquare(0, 3);
-        b->printBoard(1);
-        b->flagSquare(0, 0);
-        b->printBoard(1);
-        printf("\nRESET\n");
-        b->reset();
+        switch (c)
+        {
+        case 'r':
+            nRow = atoi(optarg);
+            break;
+        case 'c':
+            nCol = atoi(optarg);
+            break;
+        case 'b':
+            nBomb = atoi(optarg);
+            break;
+        case 'd':
+            debug = true;
+            break;
+        default:
+            usage("Unknown Arguement");
+        }
     }
+    if(nRow == -1 || nCol == -1 || nBomb == -1)
+        usage("Missing Arguemnt");
+
+    cout << "nRow(" << nRow << ") nCol(" << nCol << ") nBomb(" << nBomb << ")\n";
+    b = new Board(nRow, nCol, nBomb);
+
+    do
+    {
+        // Reset Input Variables
+        action = ' ';
+        x = y = -1;
+
+        // Print the current state of board
+
+        cout << *b << endl;
+        // Get user input
+        getline(cin, line);
+        ss.clear();
+        ss.str(line);
+        ss >> action;
+        ss >> x;
+        ss >> y;
+
+        // Perform the action
+        switch(action)
+        {
+            case 'c':
+                gState = b->clickSquare(x, y);
+                break;
+            case 'f':
+                b->flagSquare(x, y);
+                break;
+            default:
+                cout << "Action can only be one of 'c' or 'f'" << endl;
+        }
+    } while(gState == INPROGRESS);
+
+    cout << *b << endl;
+    cout << (gState == WON ? "WIN" : "LOSE") << endl;
 
     return 0;
 }
