d813393540518a1a54938e60504e023d611b91e9
John Gill <king2bgill@gmail.com>
2018-02-11 13:14:03 -0800

    Linting Fixes

--- game/mmain.cpp
+++ game/mmain.cpp
@@ -1,92 +1,88 @@
+// Copyright 2018 John Gill
 #include <string>
 #include <iostream>
 #include <sstream>
-#include "utils.h"
-#include "board.h"
+#include "../objects/utils.h"
+#include "../objects/board.h"
 
-using namespace std;
-
-int main (int argc, char **argv)
-{
-    cout.setf(ios::unitbuf);
-    cerr.setf(ios::unitbuf);
+int main(int argc, char **argv) {
+    std::cout.setf(std::ios::unitbuf);
+    std::cerr.setf(std::ios::unitbuf);
     signal(SIGSEGV, sig_handler);
 
     int x, y;
     char action;
-    Board *b;
+
     boardState_t gState = INPROGRESS;
-    stringstream ss;
-    string line;
+    std::stringstream ss;
+    std::string line;
 
     my_getargs(argc, argv);
-    if(args["nRow"] == -1 ||
+    if (args["nRow"] == -1 ||
        args["nCol"] == -1 ||
        args["nBomb"] == -1)
         usage("Missing Arguemnt");
 
-    if(args["debug"])
-        cerr << "game-" << "nRow(" << args["nRow"] << ") "
+    if (args["debug"])
+        std::cerr << "game-" << "nRow(" << args["nRow"] << ") "
              << "nCol(" << args["nCol"] << ") "
              << "nBomb(" << args["nBomb"] << ")\n";
-    b = new Board(args["nRow"], args["nCol"], args["nBomb"]);
+    Board b(args["nRow"], args["nCol"], args["nBomb"]);
 
-    //args["hardcode"] = 1;
-    //ss.clear(); ss.str("232\n***\n232");
+    // args["hardcode"] = 1;
+    // ss.clear(); ss.str("232\n***\n232");
     // ss >> *b;
 
-    do
-    {
-        do
-        {
+    do {
+        do {
             // Reset Input Variables
             action = ' ';
             x = y = -1;
 
             // Print the current state of board
-            if(args["interactive"])
-                cout << "CONTINUE" << endl << *b;
+            if (args["interactive"])
+                std::cout << "CONTINUE" << std::endl << b;
             else
-                cout << *b;
+                std::cout << b;
 
             // Get user input
-            getline(cin, line);
-            if(args["debug"])
-                cerr << "game-" << "line = " << line << endl;
+            getline(std::cin, line);
+            if (args["debug"])
+                std::cerr << "game-" << "line = " << line << std::endl;
             ss.clear();
             ss.str(line);
             ss >> action;
             ss >> x;
             ss >> y;
 
-            if(args["debug"])
-                cerr << "game-" << "Action = " << action << " x = " << x << " y = " << y << endl;
+            if (args["debug"])
+                std::cerr << "game-" << "Action = " << action <<
+                             " x = " << x << " y = " << y << std::endl;
 
             // Perform the action
-            switch(action)
-            {
+            switch (action) {
                 case 'c':
-                    gState = b->clickSquare(x, y);
+                    gState = b.clickSquare(x, y);
                     break;
                 case 'f':
-                    b->flagSquare(x, y);
+                    b.flagSquare(x, y);
                     break;
                 default:
-                    cout << "Action can only be one of 'c' or 'f'" << endl;
+                    std::cout << "Action can only be one of 'c' or 'f'" << std::endl;
             }
-        } while(gState == INPROGRESS);
+        } while (gState == INPROGRESS);
 
-        cout << (gState == WON ? "WIN" : "LOSE") << endl << *b;
-        if(args["debug"])
-            cerr << (gState == WON ? "WIN" : "LOSE") << endl;
+        std::cout << (gState == WON ? "WIN" : "LOSE") << std::endl << b;
+        if (args["debug"])
+            std::cerr << (gState == WON ? "WIN" : "LOSE") << std::endl;
 
-        getline(cin, line);
+        getline(std::cin, line);
         ss.clear();
         ss.str(line);
         ss >> action;
-        if(action == 'y')
-            b->reset();
-    } while(action == 'y');
+        if (action == 'y')
+            b.reset();
+    } while (action == 'y');
 
     return 0;
 }
